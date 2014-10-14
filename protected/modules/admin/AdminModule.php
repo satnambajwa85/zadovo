803d50a7a7cb0ded3cdeda6b7cb9d94ecf4b297e
Satnam singh <satnam.singh@venturepact.com>
2014-10-14 13:25:51 +0530

    changes

--- protected/modules/admin/AdminModule.php
+++ protected/modules/admin/AdminModule.php
@@ -6,7 +6,6 @@ class AdminModule extends CWebModule
 	{
 		// this method is called when the module is being created
 		// you may place code here to customize the module or the application
-
 		// import the module-level models and components
 		$this->setImport(array(
 			'admin.models.*',
@@ -20,6 +19,7 @@ class AdminModule extends CWebModule
 		{
 			// this method is called before any module controller action is performed
 			// you may place customized code here
+			$controller->layout = 'admin';
 			return true;
 		}
 		else
