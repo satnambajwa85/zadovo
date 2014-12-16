25fad45f26adda7da232d50e95b055810ce61e30
satnam <satnambajwa85@gmail.com>
2014-12-16 22:36:32 +0530

    changes

--- themes/home/views/site/_course.php
+++ themes/home/views/site/_course.php
@@ -5,6 +5,11 @@
 	<figure><?php echo Chtml::link('<img width="270" height="152" src="'.$path.'no-image.jpg'.'" alt="'.$data->title.'"/>',array('/site/course','id'=>$data->id));?></figure>
     <div class="details">
     <h1><?php echo CHtml::link($data->title,array('/site/course','id'=>$data->id),array('title'=>''.$data->title.''));?><span class="stars"></span></h1>
+    <div class="clear col-md-12">
+		<?php foreach($data->careerDetails as $details){?>
+    <div class="col-md-4" style="font-size:14px;margin-top:10px;"><?php echo $details->title;?></div>
+    <?php }?>
+    </div>
    </div>
    
 </article>
